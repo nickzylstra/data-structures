8005f8a52620bb5a3d6ccb18fbe055b077f8aefc
nickzylstra <nick.zylstra@gmail.com>
2019-10-24 15:59:05 -0700

    Completes tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -15,22 +15,37 @@ var treeMethods = {};
 
 treeMethods.addChild = function(value) {
   // create a new tree with argument value
+  const newTree = new Tree(value);
   // add a new tree to the children property of this
+  this.children.push(newTree);
 };
 
 treeMethods.contains = function(target) {
   // create var called treeHasTarget starts as false
+  let treeHasTarget = false;
   // if current node's value matches target
+  if (this.value === target) {
     // set treeHasTarget to true
+    treeHasTarget = true;
+  }
   // if children exists
-    // iterate through children
+  const childrenCount = this.children.length;
+  if (childrenCount > 0) {
+    // iterate through children while treeHasTarget is false
+    for (let i = 0; i < childrenCount && !treeHasTarget; i++) {
       // set treeHasTarget to return value of calling contains on child
+      treeHasTarget = this.children[i].contains(target);
+    }
+  }
 
   // return treeHasTarget
+  return treeHasTarget;
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ assuming n equals the number of nodes in tree
+ addChild - O(1), contains - O(N)
  */
