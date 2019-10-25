366276fd2f8ab56b355bb58de88883742ad4c3ac
nickzylstra <nick.zylstra@gmail.com>
2019-10-25 15:38:59 -0700

    Completes set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,34 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!this.contains(item)) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  return this._storage.indexOf(item) > -1;
 };
 
 setPrototype.remove = function(item) {
+  // set variable to index of item
+  const index = this._storage.indexOf(item);
+  // if index is -1
+  if (index === -1) {
+    // return null
+    return null;
+  }
+  // splice item index
+  this._storage.splice(index, 1);
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ N is number of items in set, all methods are O(N)
  */
