635207cd0cd39e74b740bacf008fbec36c68359f
nickzylstra <nick.zylstra@gmail.com>
2019-10-25 19:43:12 -0700

    Starts bst

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,26 @@
 var BinarySearchTree = function(value) {
+  bst = Object.create(BinarySearchTree.prototype);
+  bst._root = new Node(value);
+  return bst;
 };
 
+var Node = function (val) {
+  this.value = val;
+  this.left = null;
+  this.right = null;
+};
+
+BinarySearchTree.prototype.insert = function (value) {
+  
+};
+
+BinarySearchTree.prototype.contains = function (value) {
+
+};
+
+BinarySearchTree.prototype.depthFirstLog = function (cb) {
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
