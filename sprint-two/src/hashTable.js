96de1a405b5f61273222563896894e742775a0fa
nickzylstra <nick.zylstra@gmail.com>
2019-10-25 19:05:16 -0700

    Completes hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,20 +1,37 @@
 
 
-var HashTable = function() {
+var HashTable = function () {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  // for each element in storage
+  for (let i = 0; i < this._limit; i++) {
+    this._storage.set(i, {});
+  }
 };
 
-HashTable.prototype.insert = function(k, v) {
+HashTable.prototype.insert = function (k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // declare bucket and get bucket from storage[index]
+  const bucket = this._storage.get(index);
+  // add property k, v to bucket
+  bucket[k] = v;
+  this._storage.set(index, bucket);
 };
 
-HashTable.prototype.retrieve = function(k) {
+HashTable.prototype.retrieve = function (k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // declare bucket and assign to storage[index]
+  const bucket = this._storage.get(index);
+  // return the value at key in bucket
+  return bucket[k];
 };
 
-HashTable.prototype.remove = function(k) {
+HashTable.prototype.remove = function (k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // declare bucket and assign to storage[index]
+  const bucket = this._storage.get(index);
+  // delete the key in bucket
+  delete bucket[k];
 };
 
 
@@ -22,5 +39,3 @@ HashTable.prototype.remove = function(k) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
