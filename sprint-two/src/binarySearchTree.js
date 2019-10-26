828c88bfbb9903848d34b02b04241bb1888338c7
nickzylstra <nick.zylstra@gmail.com>
2019-10-26 10:19:10 -0700

    Completes bst

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,27 +1,71 @@
-var BinarySearchTree = function(value) {
+var BinarySearchTree = function (value) {
   bst = Object.create(BinarySearchTree.prototype);
-  bst._root = new Node(value);
+  bst.value = value;
+  bst.left = null;
+  bst.right = null;
   return bst;
 };
 
-var Node = function (val) {
-  this.value = val;
-  this.left = null;
-  this.right = null;
+BinarySearchTree.prototype.insert = function (inputValue) {
+  const inputNode = BinarySearchTree(inputValue);
+  const innerInsert = function (node) {
+    if (node.value === inputValue) {
+      return;
+    }
+    if (inputValue < node.value) {
+      if (node.left === null) {
+        node.left = inputNode;
+      } else {
+        innerInsert(node.left);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = inputNode;
+      } else {
+        innerInsert(node.right);
+      }
+    }
+  };
+  innerInsert(this);
 };
 
-BinarySearchTree.prototype.insert = function (value) {
-  
-};
-
-BinarySearchTree.prototype.contains = function (value) {
-
+BinarySearchTree.prototype.contains = function (inputValue) {
+  const innerContains = function (node) {
+    if (node.value === inputValue) {
+      return true;
+    }
+    if (inputValue < node.value) {
+      if (node.left === null) {
+        return false;
+      } else {
+        return innerContains(node.left);
+      }
+    } else {
+      if (node.right === null) {
+        return false;
+      } else {
+        return innerContains(node.right);
+      }
+    }
+  };
+  return innerContains(this);
 };
 
 BinarySearchTree.prototype.depthFirstLog = function (cb) {
-
+  const innerDFL = function (node) {
+    cb(node.value);
+    if (node.left !== null) {
+      innerDFL(node.left);
+    }
+    if (node.right !== null) {
+      innerDFL(node.right);
+    }
+  };
+  innerDFL(this);
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ assume N is number of nodes
+ insert - O(log N), cointains - O(log N), depthFirstLof - O(N).
  */
